7d816cabc2f2d0d29456fc7f5be7f1716a5575df
hcz <hcz@fb.com>
2014-09-06 17:57:53 +0100

    Added new files.

---
+++ BuildingRoutes.cpp
@@ -0,0 +1,69 @@
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <map>
+#include <cstring>
+#include <complex>
+#include <numeric>
+#include <functional>
+//#define NDEBUG
+#include <cassert>
+#include <limits>
+//INT_MAX INT_MIN
+#include <limits.h>
+#include <cstdarg>
+#include <iterator>
+#include <valarray>
+using namespace std;
+#define debug(x) cerr<<#x<<"=\""<<x<<"\""<<" at line#"<<__LINE__<<endl;
+#define Rep(i,n) for(int i=0;i<(n);++i)
+#define For(i,a,b) for(typeof(a) i=a;i<(b);++i)
+#define SZ(vec) ((int)(vec).size())
+#define All(vec) (vec).begin(),(vec).end()
+class BuildingRoutes {
+ public:
+  int build(vector <string> dist, int T) {
+    debug("Start calculation.");
+  }
+  
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"011",
+ "101",
+ "110"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; int Arg2 = 6; verify_case(0, Arg2, build(Arg0, Arg1)); }
+	void test_case_1() { string Arr0[] = {"033",
+ "309",
+ "390"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; int Arg2 = 12; verify_case(1, Arg2, build(Arg0, Arg1)); }
+	void test_case_2() { string Arr0[] = {"0123",
+ "1023",
+ "1203",
+ "1230"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; int Arg2 = 5; verify_case(2, Arg2, build(Arg0, Arg1)); }
+	void test_case_3() { string Arr0[] = {"05789654",
+ "10347583",
+ "65085479",
+ "55602398",
+ "76590934",
+ "57939045",
+ "12345608",
+ "68647640"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; int Arg2 = 40; verify_case(3, Arg2, build(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+// BEGIN CUT HERE
+int main() {
+  BuildingRoutes ___test;
+  ___test.run_test(-1);
+}
+// END CUT HERE
---
+++ RunningAroundPark.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <map>
+#include <cstring>
+#include <complex>
+#include <numeric>
+#include <functional>
+//#define NDEBUG
+#include <cassert>
+#include <limits>
+//INT_MAX INT_MIN
+#include <limits.h>
+#include <cstdarg>
+#include <iterator>
+#include <valarray>
+using namespace std;
+#define debug(x) cerr<<#x<<"=\""<<x<<"\""<<" at line#"<<__LINE__<<endl;
+#define Rep(i,n) for(int i=0;i<(n);++i)
+#define For(i,a,b) for(typeof(a) i=a;i<(b);++i)
+#define SZ(vec) ((int)(vec).size())
+#define All(vec) (vec).begin(),(vec).end()
+class RunningAroundPark {
+ public:
+  int numberOfLap(int N, vector <int> d) {
+    debug("Start calculation.");
+  }
+  
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arg0 = 3; int Arr1[] = {1,2,3}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 1; verify_case(0, Arg2, numberOfLap(Arg0, Arg1)); }
+	void test_case_1() { int Arg0 = 24; int Arr1[] = {6,6}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 2; verify_case(1, Arg2, numberOfLap(Arg0, Arg1)); }
+	void test_case_2() { int Arg0 = 3; int Arr1[] = {3,2,1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 3; verify_case(2, Arg2, numberOfLap(Arg0, Arg1)); }
+	void test_case_3() { int Arg0 = 50; int Arr1[] = {1,3,5,7,9,2,4,6,8,10}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 2; verify_case(3, Arg2, numberOfLap(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+// BEGIN CUT HERE
+int main() {
+  RunningAroundPark ___test;
+  ___test.run_test(-1);
+}
+// END CUT HERE
